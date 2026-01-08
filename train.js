134f6def03660a51b3a2d5e6b82fe0b83640db18
danielnim <xidirovdiyorbek6@gmail.com>
2026-01-08 15:26:46 +0900

    E-TASK

--- train.js
+++ train.js
@@ -59,60 +59,79 @@ mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq,
 biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
  MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
 shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.products = {
+//       non,
+//       lagmon,
+//       cola,
+//     };
+//   }
+//   getTime() {
+//     const now = new Date();
+//     const h = String(now.getHours()).padStart(2, "0");
+//     const m = String(now.getMinutes()).padStart(2, "0");
+//     return `${h}:${m}`;
+//   }
+//   qoldiq() {
+//     const time = this.getTime();
+//     const { non, lagmon, cola } = this.products;
+//     console.log(
+//       `hozir ${time}da ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
+//     );
+//   }
+//   sotish(product, amount) {
+//     const time = this.getTime();
+
+//     if (!this.products.hasOwnProperty(product)) {
+//       console.log(`${product} mahsuloti mavjud emas`);
+//       return;
+//     }
+
+//     if (this.products[product] < amount) {
+//       console.log(`${time}da yetarli ${product} yo‘q`);
+//       return;
+//     }
+
+//     this.products[product] -= amount;
+//     console.log(`${time}da ${amount}ta ${product} sotildi`);
+//   }
+//   qabul(product, amount) {
+//     const time = this.getTime();
+
+//     if (!this.products.hasOwnProperty(product)) {
+//       console.log(`${product} mahsuloti mavjud emas`);
+//       return;
+//     }
+
+//     this.products[product] += amount;
+//     console.log(`${time}da ${amount}ta ${product} qabul qilindi`);
+//   }
+// }
+
+// const newShop = new Shop(5, 5, 5);
+
+// newShop.sotish("non", 3);
+// newShop.qabul("cola", 5);
+// newShop.qoldiq();
 */
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.products = {
-      non,
-      lagmon,
-      cola,
-    };
-  }
-  getTime() {
-    const now = new Date();
-    const h = String(now.getHours()).padStart(2, "0");
-    const m = String(now.getMinutes()).padStart(2, "0");
-    return `${h}:${m}`;
-  }
-  qoldiq() {
-    const time = this.getTime();
-    const { non, lagmon, cola } = this.products;
-    console.log(
-      `hozir ${time}da ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
-    );
-  }
-  sotish(product, amount) {
-    const time = this.getTime();
+// C-TASK
+/*
+Shunday function tuzing, u bitta string 
+argumentni qabul qilib osha stringni teskari 
+qilib return qilsin. MASALAN: getReverse("hello") return qilsin "olleh".
+*/
 
-    if (!this.products.hasOwnProperty(product)) {
-      console.log(`${product} mahsuloti mavjud emas`);
-      return;
-    }
-
-    if (this.products[product] < amount) {
-      console.log(`${time}da yetarli ${product} yo‘q`);
-      return;
-    }
-
-    this.products[product] -= amount;
-    console.log(`${time}da ${amount}ta ${product} sotildi`);
+function getReverse(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    arr.push(str[i]);
   }
-  qabul(product, amount) {
-    const time = this.getTime();
-
-    if (!this.products.hasOwnProperty(product)) {
-      console.log(`${product} mahsuloti mavjud emas`);
-      return;
-    }
-
-    this.products[product] += amount;
-    console.log(`${time}da ${amount}ta ${product} qabul qilindi`);
-  }
+  arr.reverse();
+  let new_arr = arr.join("");
+  console.log(new_arr);
 }
 
-const newShop = new Shop(5, 5, 5);
-
-newShop.sotish("non", 3);
-newShop.qabul("cola", 5);
-newShop.qoldiq();
+getReverse("hello");
